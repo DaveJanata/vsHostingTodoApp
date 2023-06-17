6a6074577b815cb3f3fe33c955bd836b5b015b86
DaveJ <janatadavid.95@gmail.com>
2023-06-17 14:52:25 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vs-hosting-todo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'vs-hosting-todo';
+}
